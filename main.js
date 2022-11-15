42681c7cb80d88eb1af83e384ab68feae527e2ed
Anna Różańska <a.rozanska.poczta@gmail.com>
2022-11-15 17:02:07 +0100

    fixed main.js

--- main.js
+++ main.js
@@ -9,34 +9,33 @@ const main = () => {
 
 const URL = 'https://api.nbp.pl/api/exchangerates/rates/a/';
 
-const calc = (i) => {
-	const num = inputCash.value * i;
-	return num.toFixed(2);
+const convertCurrency = (rate) => {
+	const result = inputCash.value * rate;
+	return result.toFixed(2);
 };
 
 const converter = () => {
 	const valueNum = inputCash.value;
 	const valueOption = option.value;
 
-	if (valueNum <= 0 || valueNum == '') {
+	if (!valueNum || valueNum === '') {
 		alert('Wprowadź kwotę');
 	} else {
 		axios
 			.get(URL + `${valueOption}/`)
 			.then((res) => {
-				const eur = res.data.rates[0].mid;
-				const usd = res.data.rates[0].mid;
-				const chf = res.data.rates[0].mid;
+
+				const rate = res.data.rates[0].mid;
 
 				if (valueOption == 'EUR') {
-					h1.innerHTML = `to: ${calc(eur)} PLN`;
+					h1.innerHTML = `to: ${convertCurrency(rate)} PLN`;
 				} else if (valueOption == 'USD') {
-					h1.innerHTML = `to: ${calc(usd)} PLN`;
+					h1.innerHTML = `to: ${convertCurrency(rate)} PLN`;
 				} else if (valueOption == 'CHF') {
-					h1.innerHTML = `to: ${calc(chf)} PLN`;
+					h1.innerHTML = `to: ${convertCurrency(rate)} PLN`;
 				}
 			})
-			.catch(() => alert('Wprowadź kwotę'));
+			.catch(() => alert('Niestety w tej chwili nie udało nam się pobrać kursu wybranej waluty, spróbuj jeszcze raz.'));
 	}
 };
 
